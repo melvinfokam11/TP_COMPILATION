621a1d0a0e5056faf586974f685c87014f50b6fe
Vilmo18 <yvan.vilmorin@facsciences-uy1.cm>
2023-06-08 16:47:23 +0100

    push

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 a={
-    'alphabet':['a'],
-    'state':[1,2],
-    'initialstate':[1],
-    'finalstate':[2],
-    'transition':[[1,'e',1],[1,'a',2],[2,'a',2]]
+    'alphabet':['0','1'],
+    'state':['a','b','c','d'],
+    'initialstate':['a'],
+    'finalstate':['b'],
+    'transition':[['a','0','a'],['a','0','b'],['a','1','a'],
+                  ['b','1','c'],['c','0','d'],]
 }
 def epsilonAFN(a):
     print(a['transition'])
@@ -13,3 +14,41 @@ def epsilonAFN(a):
     return 'Ce n est pas un epsilon AFN'
 
 print(epsilonAFN(a))
+
+def is_AFN_or_AFD(a):
+    table=[]
+    for t in a['transition']:
+        chain=''.join(t)
+        table.append(chain)
+    print(table)
+    for state in a['state']:
+        for alpha in a['alphabet']:
+            val=0
+            for word in table:
+                if state+alpha in word:
+                   val=val+1
+            #print(state+alpha,' ',val)
+            if(val>1):
+                return 'It is a AFN'
+    return 'It is a AFD'
+"""""
+def is_AFD(a):
+    table=[]
+    for t in a['transition']:
+        chain=''.join(t)
+        table.append(chain)
+    print(table)
+    for state in a['state']:
+        for alpha in a['alphabet']:
+            val=0
+            for word in table:
+                if state+alpha in word:
+                   val=val+1
+            #print(state+alpha,' ',val)
+            if(val>1):
+                return 'It is not a AFD'
+    return 'It is a AFD'
+"""
+
+print(is_AFN_or_AFD(a))
+
